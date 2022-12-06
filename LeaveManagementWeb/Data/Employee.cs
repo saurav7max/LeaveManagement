289993c7a6a71e744bf5101be5ca99aa37e9d7f4
sauravp <sauravp@cybage.com>
2022-12-06 15:50:17 +0530

    Extended Employee as IdentityUser

---
+++ LeaveManagementWeb/Data/Employee.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace LeaveManagementWeb.Data
+{
+    public class Employee : IdentityUser
+    {
+        public string? Firstname { get; set; }
+
+        public string? Lastname { get; set; }
+
+        public DateTime DOB { get; set; }
+
+        public string? TaxId { get; set; }  
+
+        public DateTime DOJ { get; set; }
+        
+    }
+}
---
+++ LeaveManagementWeb/Data/Migrations/20221206064313_add-migration extendedEmployee.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LeaveManagementWeb.Data.Migrations
+{
+    public partial class addmigrationextendedEmployee : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DOB",
+                table: "AspNetUsers",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DOJ",
+                table: "AspNetUsers",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Firstname",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Lastname",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "TaxId",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DOB",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "DOJ",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Firstname",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Lastname",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "TaxId",
+                table: "AspNetUsers");
+        }
+    }
+}
